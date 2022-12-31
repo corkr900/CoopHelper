fe45a9e09cd61112faded233fc5b8e54a0bcdea4
corkr900 <109830893+corkr900@users.noreply.github.com>
2022-12-30 22:05:38 -0600

    fix crash on connect

--- Code/CoopHelper/CoopHelper/IO/CNetComm.cs
+++ Code/CoopHelper/CoopHelper/IO/CNetComm.cs
@@ -167,7 +167,7 @@ namespace Celeste.Mod.CoopHelper.IO {
 		}
 
 		internal void Tick(ulong counter) {
-			if (CoopHelperModule.Session.IsInCoopSession && EntityStateTracker.HasUpdates) {
+			if (CoopHelperModule.Session?.IsInCoopSession == true && EntityStateTracker.HasUpdates) {
 				EntityStateTracker.NotifyInitiatingOutgoingMessage();
 				Send(new DataBundledEntityUpdate(), false);
 			}
