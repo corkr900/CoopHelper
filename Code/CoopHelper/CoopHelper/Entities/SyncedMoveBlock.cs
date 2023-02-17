8688dbc1bcf2d8ccaf49e25adfb14004cabc9a84
corkr900 <109830893+corkr900@users.noreply.github.com>
2023-02-17 13:20:31 -0600

    start desync protection fix

--- Code/CoopHelper/CoopHelper/Entities/SyncedMoveBlock.cs
+++ Code/CoopHelper/CoopHelper/Entities/SyncedMoveBlock.cs
@@ -21,6 +21,8 @@ namespace Celeste.Mod.CoopHelper.Entities {
 		private float otherPlayerTotalMovement = 0;
 		private MovementState lastState = MovementState.Idling;
 		internal bool LastMoveCheckResult = false;
+		private MovementTriggeredBy triggeredBy = MovementTriggeredBy.None;
+		private bool waitingForBreakConfirmation = false;
 
 		private object syncDeltaLock = new object();
 		private float mySyncedMovement = 0;
@@ -31,6 +33,14 @@ namespace Celeste.Mod.CoopHelper.Entities {
 			Idling = 0,
 			Moving = 1,
 			Breaking = 2,
+		}
+
+		private enum MovementTriggeredBy
+		{
+			None = 0,
+			Me = 1,
+			SomeoneElse = 2,
+			Simultaneous = 3,
 		}
 
 		public SyncedMoveBlock(EntityData data, Vector2 offset) : base(data, offset) {
@@ -46,16 +56,218 @@ namespace Celeste.Mod.CoopHelper.Entities {
 			MovementState state = dd.Get<MovementState>("state");
 			if (state != MovementState.Moving) {
 				otherPlayerTotalMovement = 0;
-				mySyncedMovement = 0f;
+				triggeredBy = MovementTriggeredBy.None;
 				lock (syncDeltaLock) {
 					mySyncedMovement = 0;
 				}
 			}
 			if (state != lastState) {
 				lastState = state;
+				if (state == MovementState.Moving && triggeredBy == MovementTriggeredBy.None) {
+					triggeredBy = MovementTriggeredBy.Me;
+				}
 				EntityStateTracker.PostUpdate(this);
 			}
 		}
+
+		//private IEnumerator Controller()
+		//{
+		//	while (true)
+		//	{
+		//		triggered = false;
+		//		state = MovementState.Idling;
+		//		while (!triggered && !HasPlayerRider())
+		//		{
+		//			yield return null;
+		//		}
+		//		Audio.Play("event:/game/04_cliffside/arrowblock_activate", Position);
+		//		state = MovementState.Moving;
+		//		StartShaking(0.2f);
+		//		ActivateParticles();
+		//		yield return 0.2f;
+		//		targetSpeed = (fast ? 75f : 60f);
+		//		moveSfx.Play("event:/game/04_cliffside/arrowblock_move");
+		//		moveSfx.Param("arrow_stop", 0f);
+		//		StopPlayerRunIntoAnimation = false;
+		//		float crashTimer = 0.15f;
+		//		float crashResetTimer = 0.1f;
+		//		float noSteerTimer = 0.2f;
+		//		while (true)
+		//		{
+		//			if (canSteer)
+		//			{
+		//				targetAngle = homeAngle;
+		//				bool flag = ((direction != Directions.Right && direction != 0) ? HasPlayerClimbing() : HasPlayerOnTop());
+		//				if (flag && noSteerTimer > 0f)
+		//				{
+		//					noSteerTimer -= Engine.DeltaTime;
+		//				}
+		//				if (flag)
+		//				{
+		//					if (noSteerTimer <= 0f)
+		//					{
+		//						if (direction == Directions.Right || direction == Directions.Left)
+		//						{
+		//							targetAngle = homeAngle + (float)Math.PI / 4f * (float)angleSteerSign * (float)Input.MoveY.Value;
+		//						}
+		//						else
+		//						{
+		//							targetAngle = homeAngle + (float)Math.PI / 4f * (float)angleSteerSign * (float)Input.MoveX.Value;
+		//						}
+		//					}
+		//				}
+		//				else
+		//				{
+		//					noSteerTimer = 0.2f;
+		//				}
+		//			}
+		//			if (Scene.OnInterval(0.02f))
+		//			{
+		//				MoveParticles();
+		//			}
+		//			speed = Calc.Approach(speed, targetSpeed, 300f * Engine.DeltaTime);
+		//			angle = Calc.Approach(angle, targetAngle, (float)Math.PI * 16f * Engine.DeltaTime);
+		//			Vector2 vector = Calc.AngleToVector(angle, speed);
+		//			Vector2 vec = vector * Engine.DeltaTime;
+		//			bool flag2;
+		//			if (direction == Directions.Right || direction == Directions.Left)
+		//			{
+		//				flag2 = MoveCheck(vec.XComp());
+		//				noSquish = Scene.Tracker.GetEntity<Player>();
+		//				MoveVCollideSolids(vec.Y, thruDashBlocks: false);
+		//				noSquish = null;
+		//				LiftSpeed = vector;
+		//				if (Scene.OnInterval(0.03f))
+		//				{
+		//					if (vec.Y > 0f)
+		//					{
+		//						ScrapeParticles(Vector2.UnitY);
+		//					}
+		//					else if (vec.Y < 0f)
+		//					{
+		//						ScrapeParticles(-Vector2.UnitY);
+		//					}
+		//				}
+		//			}
+		//			else
+		//			{
+		//				flag2 = MoveCheck(vec.YComp());
+		//				noSquish = Scene.Tracker.GetEntity<Player>();
+		//				MoveHCollideSolids(vec.X, thruDashBlocks: false);
+		//				noSquish = null;
+		//				LiftSpeed = vector;
+		//				if (Scene.OnInterval(0.03f))
+		//				{
+		//					if (vec.X > 0f)
+		//					{
+		//						ScrapeParticles(Vector2.UnitX);
+		//					}
+		//					else if (vec.X < 0f)
+		//					{
+		//						ScrapeParticles(-Vector2.UnitX);
+		//					}
+		//				}
+		//				if (direction == Directions.Down && Top > (float)(SceneAs<Level>().Bounds.Bottom + 32))
+		//				{
+		//					flag2 = true;
+		//				}
+		//			}
+		//			if (flag2)
+		//			{
+		//				moveSfx.Param("arrow_stop", 1f);
+		//				crashResetTimer = 0.1f;
+		//				if (!(crashTimer > 0f))
+		//				{
+		//					break;
+		//				}
+		//				crashTimer -= Engine.DeltaTime;
+		//			}
+		//			else
+		//			{
+		//				moveSfx.Param("arrow_stop", 0f);
+		//				if (crashResetTimer > 0f)
+		//				{
+		//					crashResetTimer -= Engine.DeltaTime;
+		//				}
+		//				else
+		//				{
+		//					crashTimer = 0.15f;
+		//				}
+		//			}
+		//			Level level = Scene as Level;
+		//			if (Left < (float)level.Bounds.Left || Top < (float)level.Bounds.Top || Right > (float)level.Bounds.Right)
+		//			{
+		//				break;
+		//			}
+		//			yield return null;
+		//		}
+		//		Audio.Play("event:/game/04_cliffside/arrowblock_break", Position);
+		//		moveSfx.Stop();
+		//		state = MovementState.Breaking;
+		//		speed = (targetSpeed = 0f);
+		//		angle = (targetAngle = homeAngle);
+		//		StartShaking(0.2f);
+		//		StopPlayerRunIntoAnimation = true;
+		//		yield return 0.2f;
+		//		BreakParticles();
+		//		List<Debris> debris = new List<Debris>();
+		//		for (int i = 0; (float)i < Width; i += 8)
+		//		{
+		//			for (int j = 0; (float)j < Height; j += 8)
+		//			{
+		//				Vector2 vector2 = new Vector2((float)i + 4f, (float)j + 4f);
+		//				Debris debris2 = Engine.Pooler.Create<Debris>().Init(Position + vector2, Center, startPosition + vector2);
+		//				debris.Add(debris2);
+		//				Scene.Add(debris2);
+		//			}
+		//		}
+		//		MoveBlock moveBlock = this;
+		//		Vector2 amount = startPosition - Position;
+		//		DisableStaticMovers();
+		//		moveBlock.MoveStaticMovers(amount);
+		//		Position = startPosition;
+		//		Visible = (Collidable = false);
+		//		yield return 2.2f;
+		//		foreach (Debris item in debris)
+		//		{
+		//			item.StopMoving();
+		//		}
+		//		while (CollideCheck<Actor>() || CollideCheck<Solid>())
+		//		{
+		//			yield return null;
+		//		}
+		//		Collidable = true;
+		//		EventInstance instance = Audio.Play("event:/game/04_cliffside/arrowblock_reform_begin", debris[0].Position);
+		//		MoveBlock moveBlock2 = this;
+		//		Coroutine component;
+		//		Coroutine routine = (component = new Coroutine(SoundFollowsDebrisCenter(instance, debris)));
+		//		moveBlock2.Add(component);
+		//		foreach (Debris item2 in debris)
+		//		{
+		//			item2.StartShaking();
+		//		}
+		//		yield return 0.2f;
+		//		foreach (Debris item3 in debris)
+		//		{
+		//			item3.ReturnHome(0.65f);
+		//		}
+		//		yield return 0.6f;
+		//		routine.RemoveSelf();
+		//		foreach (Debris item4 in debris)
+		//		{
+		//			item4.RemoveSelf();
+		//		}
+		//		Audio.Play("event:/game/04_cliffside/arrowblock_reappear", Position);
+		//		Visible = true;
+		//		EnableStaticMovers();
+		//		speed = (targetSpeed = 0f);
+		//		angle = (targetAngle = homeAngle);
+		//		noSquish = null;
+		//		fillColor = idleBgFill;
+		//		UpdateColors();
+		//		flash = 1f;
+		//	}
+		//}
 
 		public override void Added(Scene scene) {
 			base.Added(scene);
@@ -84,6 +296,7 @@ namespace Celeste.Mod.CoopHelper.Entities {
 		public static SyncedMoveBlockState ParseState(CelesteNetBinaryReader r) {
 			SyncedMoveBlockState s = new SyncedMoveBlockState();
 			s.Moving = r.ReadBoolean();
+			s.InitialTrigger = r.ReadBoolean();
 			s.Position = r.ReadVector2();
 			s.MovementDelta = r.ReadSingle();
 			return s;
@@ -92,10 +305,18 @@ namespace Celeste.Mod.CoopHelper.Entities {
 		public void ApplyState(object state) {
 			if (state is SyncedMoveBlockState mbs) {
 				DynamicData dd = new DynamicData(this);
+				// Handle other player triggering the block
 				if (lastState == MovementState.Idling && mbs.Moving) {
 					dd.Set("triggered", true);
 					lastState = MovementState.Moving;
+					triggeredBy = MovementTriggeredBy.SomeoneElse;
 				}
+				// Handle simultaneous trigger
+				if (triggeredBy == MovementTriggeredBy.Me && mbs.InitialTrigger)
+				{
+					triggeredBy = MovementTriggeredBy.Simultaneous;
+				}
+				// Handle steering updates
 				if (canSteer && lastState == MovementState.Moving && mbs.Moving) {
 					if (LastMoveCheckResult && PositionIsFartherMovement(mbs.Position)) {
 						otherPlayerTotalMovement += MovesVertically ? (mbs.Position - Position).X : (mbs.Position - Position).Y;
@@ -132,6 +353,7 @@ namespace Celeste.Mod.CoopHelper.Entities {
 		public void WriteState(CelesteNetBinaryWriter w) {
 			if (canSteer) {
 				w.Write(lastState == MovementState.Moving);
+				w.Write(triggeredBy == MovementTriggeredBy.Me);
 				w.Write(Position);
 				lock (syncDeltaLock) {
 					float deltaMovement = 0f;
@@ -146,6 +368,7 @@ namespace Celeste.Mod.CoopHelper.Entities {
 			}
 			else {
 				w.Write(lastState == MovementState.Moving);
+				w.Write(triggeredBy == MovementTriggeredBy.Me);
 				w.Write(Position);
 				w.Write(0f);
 			}
@@ -154,6 +377,7 @@ namespace Celeste.Mod.CoopHelper.Entities {
 
 	public class SyncedMoveBlockState {
 		public bool Moving;
+		public bool InitialTrigger;
 		public Vector2 Position;
 		public float MovementDelta;
 	}
